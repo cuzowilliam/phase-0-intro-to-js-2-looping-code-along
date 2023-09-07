869137a77d23f8c92feb338398fad777d281c433
william <cuzowilliam@yahoo.com>
2023-09-07 00:34:44 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const messages = [];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return messages;
+}
+
+var number = 10;
+
+function countDown(number) {
+    while (number >= 0) {
+    console.log(number);
+    number--;
+
+    debugger;
+    }
+}
